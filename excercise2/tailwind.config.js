1d554b82ed52ecb1275e9336c459c4ca8f6e1b6a
Zamirzamir44 <tooradicalxxi@gmail.com>
2021-01-08 18:42:36 +0100

    carousel slider added

--- excercise2/tailwind.config.js
+++ excercise2/tailwind.config.js
@@ -17,6 +17,17 @@ module.exports = {
         'primary': '#FFFDFD',
          'secondary': '#988787',
          'tertiary': '#BDB2B2',
+         black: {
+          '100': '#444444',
+          '200': '#5bb7d9',
+          '300': '#43add4',
+          '400': '#2ca2ce',
+          '500': '#1498c9',
+          '600': '#1289b5',
+          '700': '#107aa1',
+          '800': '#0e6a8d',
+          '900': '#0c5b79'
+        },
          
        },
        fontFamily: {
@@ -53,6 +64,9 @@ module.exports = {
   variants: {
     extend: {
     },
+  },
+  corePlugins: {
+    container: false,
   },
   plugins: [
     require ('tailwind-bootstrap-grid')({
